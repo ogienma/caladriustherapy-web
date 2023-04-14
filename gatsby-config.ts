7fc10dff100b015be203ebf73fc2107a8f8fe155
Maxim Ogienko <maximogienko@gmail.com>
2023-04-13 22:22:14 -0400

    initial commit

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,7 +9,10 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-google-gtag", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+  plugins: [
+    // TODO: read https://www.gatsbyjs.com/plugins/gatsby-plugin-google-gtag/ and setup google analytics
+    // "gatsby-plugin-google-gtag",
+   "gatsby-plugin-image", "gatsby-plugin-sitemap", {
     resolve: 'gatsby-plugin-manifest',
     options: {
       "icon": "src/images/icon.png"

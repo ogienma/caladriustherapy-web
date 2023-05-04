12fef70a2bf8b3fae3c2dc3b8634d95bce7b66d7
Maxim Ogienko <maximogienko@gmail.com>
2023-05-03 21:39:54 -0400

    new colors

--- src/components/OurTeamPanel.tsx
+++ src/components/OurTeamPanel.tsx
@@ -40,7 +40,7 @@ const Headshot = ({ member }: HeadshotProps) => {
 export interface IProps {}
 
 const OurTeamPanel: React.FC<IProps> = () => {
-  const members = teamData.members.reverse();
+  const members = teamData.members.toReversed();
   return (
     <>
       <section className="wrapper style1 align-center">

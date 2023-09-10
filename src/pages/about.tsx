08f0cffca37484103546ad0c7e63148729d38eb5
Maxim Ogienko <maximogienko@gmail.com>
2023-09-10 15:22:16 -0400

    fix letters

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -29,10 +29,7 @@ const AboutPage: React.FC<PageProps> = () => {
               <div className="content">
               <span><h2 style={{display: "inline"}}>{member.name}
               {member.letters && (
-                <>
-                  <h2>,</h2>
-                  <h3 style={{display: "inline", marginLeft: "0.25em"}}>{member.letters}</h3>
-                </>
+                <>, {member.letters}</>
               )}</h2>
               </span>
               {/* <span><h2 style={{display: "inline"}}>{member.name}</h2><h3 style={{display: "inline", marginLeft: "0.25em", color: "#aeaeae"}}>{member.letters}</h3></span> */}

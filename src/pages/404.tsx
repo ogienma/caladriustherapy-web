61835a25ef6e52bb4731e6aeabf94bd5e39e897a
ogienma <maximogienko@gmail.com>
2024-04-29 18:48:23 -0400

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,6 +3,8 @@ import { Link } from "gatsby"
 import type { HeadFC, PageProps } from "gatsby";
 import { SEO } from "../components/SEO";
 import Layout from "../components/Layout";
+import { Notification } from '@mantine/core';
+
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <>
@@ -15,6 +17,9 @@ const NotFoundPage: React.FC<PageProps> = () => {
             <section className="spotlight style2 orient-right content-align-left image-position-center about-member-section">
             Sorry 😔, we couldn't find what you were looking for.
             </section>
+        <Notification color="pink" title="We are moving">
+      Starting with May 20th, we will be located at <a href="https://maps.app.goo.gl/gnV7EPLfCnsUMzNd8">200 Queens Road, Suite 300 in Charlotte</a>!
+    </Notification>
       </Layout>
     </>
   );

45ab24f7d3f28826eb274bd3dfc0b8c52c430daf
ogienma <maximogienko@gmail.com>
2024-04-29 18:56:34 -0400

    Update 404.tsx

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -3,11 +3,16 @@ import { Link } from "gatsby"
 import type { HeadFC, PageProps } from "gatsby";
 import { SEO } from "../components/SEO";
 import Layout from "../components/Layout";
-import { Notification } from '@mantine/core';
+import {
+  Alert,
+} from '@chakra-ui/react'
 
 const NotFoundPage: React.FC<PageProps> = () => {
   return (
     <>
+        <Alert status='warning'>
+     <strong>We are moving!</strong>Starting with May 20th, we will be located at <a href="https://maps.app.goo.gl/gnV7EPLfCnsUMzNd8">200 Queens Road, Suite 300 in Charlotte</a>.
+  </Alert>
       <Layout>
         <section className="wrapper style1 align-center">
           <div className="inner" style={{padding: "6rem 2.5rem 2.5rem 2.5rem"}}>
@@ -17,9 +22,6 @@ const NotFoundPage: React.FC<PageProps> = () => {
             <section className="spotlight style2 orient-right content-align-left image-position-center about-member-section">
             Sorry 😔, we couldn't find what you were looking for.
             </section>
-        <Notification color="pink" title="We are moving">
-      Starting with May 20th, we will be located at <a href="https://maps.app.goo.gl/gnV7EPLfCnsUMzNd8">200 Queens Road, Suite 300 in Charlotte</a>!
-    </Notification>
       </Layout>
     </>
   );
